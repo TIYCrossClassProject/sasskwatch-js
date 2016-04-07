bf590345854cb8d10230a9bf13c7e23391ccdc35
John Cooley <johndavidcooley@gmail.com>
2016-04-07 11:12:08 -0400

    Made login_view page.

---
+++ src/js/login_view.js
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from React;
+
+export default class LoginView extends Component {
+
+	static propTypes = {
+		onLoginToSignup = PropTypes.func.isRequired,
+		onLoginSubmit = PropTypes.func.isRequired
+	}
+
+	render() {
+		return (
+			<div className="login-view">
+				<SSF onData={dataHandler}>
+					<input type="text" name="username" placeholder=" Enter Your Username"></input>
+					<input type="password" name="password" placeholder=" Enter Your Password"></input>
+					<button>Log In</button>
+				</SSF>
+			</div>
+		);
+	}
+}
