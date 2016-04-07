04d76ddd942eae8b473654e732cd9a5fb8b1b8f4
John Cooley <johndavidcooley@gmail.com>
2016-04-07 14:19:28 -0400

    Made a fake 'Jason' JSON file for testing.

---
+++ src/js/temp_jason.js
@@ -0,0 +1,43 @@
+export default [
+	{
+		firstName: 'Jason',
+		lastName: 'Bateman',
+		username: 'jasonbateman',
+		password: 'batin',
+		email: 'jason@bateman.com',
+		image: 'http://fillmurray.com/50/50'
+	},
+
+	{
+		firstName: 'Jason',
+		lastName: 'Voorhees',
+		username: 'jasonvoorhees',
+		password: 'hockeymask',
+		email: 'jason@voorhees.com',
+		image: 'http://fillmurray.com/50/50'
+	},
+	{
+		firstName: 'Jason',
+		lastName: 'Williams',
+		username: 'jasonwilliams',
+		password: 'basketball',
+		email: 'jason@williams.com',
+		image: 'http://fillmurray.com/50/50'
+	},
+	{
+		firstName: 'Jason',
+		lastName: 'Biggs',
+		username: 'jasonbiggs',
+		password: 'applepie',
+		email: 'jason@biggs.com',
+		image: 'http://fillmurray.com/50/50'
+	},
+	{
+		firstName: 'Jason',
+		lastName: 'Derulo',
+		username: 'jasonderulo',
+		password: 'ridinsolo',
+		email: 'jason@derulo.com',
+		image: 'http://fillmurray.com/50/50'
+	},
+]
